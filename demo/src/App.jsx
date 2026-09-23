b3b1f620a3123e94bc86c6aea90cc1b91455612f
Siddharth Shinde <siddhushinde788@gmail.com>
2026-09-23 20:16:06 +0000

    fix: remove duplicate nested GoogleOAuthProvider which crashes react hooks context

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -306,8 +306,7 @@ export default function App() {
   }
 
   return (
-    <GoogleOAuthProvider clientId={googleClientId || '249182249330-bjd3rilnsokh19463u3ra8ekojkb3q34.apps.googleusercontent.com'}>
-      <div className="flex w-full h-full relative">
+    <div className="flex w-full h-full relative">
         {renderConfigOverlay()}
       {/* LEFT HALF */}
       <div className="w-1/2 p-12 flex flex-col justify-center border-r border-gray-800">
@@ -412,6 +411,5 @@ export default function App() {
         </div>
       </div>
     </div>
-    </GoogleOAuthProvider>
   )
 }
